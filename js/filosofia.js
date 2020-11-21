fd667bba45fb1314d71d90d2d47aa4b1065efffd
pieraasenjo-tdp <piera.asenjo@telefonica.com>
2020-11-21 14:37:43 -0500

    first commit

---
+++ js/filosofia.js
@@ -0,0 +1,36 @@
+function ocultarMision(x) {
+  x.style.display = "none";
+const misionHover = document.getElementById('mision_container')
+misionHover.style.display = 'flex'
+}
+
+function mostrarMision(x) {
+  x.style.display = "flex";
+  const misionHover = document.getElementById('mision_container')
+misionHover.style.display = 'none'
+}
+
+function ocultarVision(x) {
+  x.style.display = "none";
+const misionHover = document.getElementById('vision_container')
+misionHover.style.display = 'flex'
+}
+
+function mostrarVision(x) {
+  x.style.display = "flex";
+  const misionHover = document.getElementById('vision_container')
+misionHover.style.display = 'none'
+}
+
+
+function ocultarValores(x) {
+  x.style.display = "none";
+const misionHover = document.getElementById('valores_container')
+misionHover.style.display = 'flex'
+}
+
+function mostrarValores(x) {
+  x.style.display = "flex";
+  const misionHover = document.getElementById('valores_container')
+misionHover.style.display = 'none'
+}
