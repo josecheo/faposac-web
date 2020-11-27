818b4a71b286cfd0e56f1e0eac4ac0fe17280fab
joseche0 <joseche0.ja@gmail.com>
2020-11-26 23:04:30 -0500

    res

--- js/filosofia.js
+++ js/filosofia.js
@@ -1,36 +1,59 @@
-function ocultarMision(x) {
-  x.style.display = "none";
-const misionHover = document.getElementById('mision_container')
-misionHover.style.display = 'flex'
+function ocultarMision(element) {
+  const mision_container_active = document.getElementById('mision_container_active')
+  const mision_container = document.getElementById('mision_container')
+
+  const vision_container_active = document.getElementById('vision_container_active')
+  const vision_container = document.getElementById('vision_container')
+
+    const valores_container_active = document.getElementById('valores_container_active')
+  const valores_container = document.getElementById('valores_container')
+
+  if (element === 'Mision') {
+    mision_container_active.style.display = "flex";
+    mision_container.style.display = 'none'
+  }
+    if (element === 'Vision') {
+    vision_container_active.style.display = "flex";
+    vision_container.style.display = 'none'
+   }
+      if (element === 'Valores') {
+        valores_container_active.style.display = "flex";
+        valores_container.style.display = 'none'
+  }
+
 }
 
-function mostrarMision(x) {
-  x.style.display = "flex";
-  const misionHover = document.getElementById('mision_container')
-misionHover.style.display = 'none'
+function mostrarMision(element) {
+    const mision_container_active = document.getElementById('mision_container_active')
+  const mision_container = document.getElementById('mision_container')
+
+  const vision_container_active = document.getElementById('vision_container_active')
+  const vision_container = document.getElementById('vision_container')
+
+  const valores_container_active = document.getElementById('valores_container_active')
+  const valores_container = document.getElementById('valores_container')
+
+  if (element === 'Mision') {
+        mision_container_active.style.display = "none";
+        mision_container.style.display = 'flex'
+  }
+
+    if (element === 'Vision') {
+        vision_container_active.style.display = "none";
+        vision_container.style.display = 'flex'
+  }
+
+      if (element === 'Valores') {
+        valores_container_active.style.display = "none";
+        valores_container.style.display = 'flex'
+  }
 }
 
-function ocultarVision(x) {
-  x.style.display = "none";
-const misionHover = document.getElementById('vision_container')
-misionHover.style.display = 'flex'
-}
 
-function mostrarVision(x) {
-  x.style.display = "flex";
-  const misionHover = document.getElementById('vision_container')
-misionHover.style.display = 'none'
-}
+function MostrarMenu() { 
 
+  const nav_ul_respon = document.getElementById('nav_ul_respon')
+ 
+  nav_ul_respon.classList.toggle('nav_ul_respon-active')
 
-function ocultarValores(x) {
-  x.style.display = "none";
-const misionHover = document.getElementById('valores_container')
-misionHover.style.display = 'flex'
-}
-
-function mostrarValores(x) {
-  x.style.display = "flex";
-  const misionHover = document.getElementById('valores_container')
-misionHover.style.display = 'none'
 }
